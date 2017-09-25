7c316f2c4efa9c614a4dc6225bbc4ac002e9e2d2
jakerutl <j_rutledge3@fanshaweonline.ca>
2017-09-25 15:24:31 -0400

    add all my portfolio files

---
+++ nav.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+    <meta name="keywords" content="Jake, Jacob, Rutlegde, Portfolio">
+    <meta name="author" content="Jake Rutledge">
+    <title>Portfolio Coming Soon!</title>
+  <!-- <link rel="stylesheet" href="css/reset.css" type="text/css" media="screen"> -->
+  <link rel="stylesheet" href="css/main.css" type="text/css">
+</head>
+<body>
+
+
+<nav>
+  <div class="navigation">
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="gallery.php">Gallery</a></li>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="index.php">Home</a></li>
+    </ul>
+  </div>
+</nav>
