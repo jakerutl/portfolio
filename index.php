67d10d218b977539d25b98e519c775fc5163edb3
jakerutl <j_rutledge3@fanshaweonline.ca>
2017-09-30 19:27:00 -0400

    made the index more mobile friendly

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 
 <html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="initial-scale=1.0, width=device-width"/>
 <link rel="stylesheet" href="css/main.css">
+</head>
 <body bgcolor="#4487CC">
   <div class="homeLogo">
 <img src="images/new_logo.png" alt="logo">
