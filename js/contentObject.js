77a281adea62fdf4f885283f1237186cfc16a218
jakerutl <j_rutledge3@fanshaweonline.ca>
2017-11-07 19:22:57 -0500

    changed some buttons, neanearly completed mobile version

--- js/contentObject.js
+++ js/contentObject.js
@@ -1,6 +1,5 @@
 var dynamicContent = {
 		spring : {
-			text : "Spring is one of the four conventional temperate seasons, following winter and preceding summer. There are various technical definitions of spring, but local usage of the term varies according to local climate, cultures and customs. When it is spring in the northern hemisphere, it will be autumn in the southern hemisphere. At the spring equinox, days are close to 12 hours long with day length increasing as the season progresses. Spring and 'springtime' refer to the season, and also to ideas of rebirth, rejuvenation, renewal, resurrection, and regrowth.",
 
 			headline : "Spring",
 
@@ -33,7 +32,6 @@ var dynamicContent = {
 		},
 
 		lightboxTwo: {
-			text : "Spring is one of the four conventional temperate seasons, following winter and preceding summer. There are various technical definitions of spring, but local usage of the term varies according to local climate, cultures and customs. When it is spring in the northern hemisphere, it will be autumn in the southern hemisphere. At the spring equinox, days are close to 12 hours long with day length increasing as the season progresses. Spring and 'springtime' refer to the season, and also to ideas of rebirth, rejuvenation, renewal, resurrection, and regrowth.",
 
 			headline : "Spring",
 
@@ -47,11 +45,10 @@ var dynamicContent = {
 
 			imageDescription : [
 				"This is an assignment that required the students to design a tattoo in photoshop/illustrator and photoshop it onto our bodies.",
-				"Have you ever seen a baby duck more adorable than this little fella? I didn't think so.",
+				"We had to recreate a poster staring Derrick Rose.",
 				"This was a group project were a team of three was required to design a landing page for a fictional movie.",
 				"This is a poster of a retouched model with an illustrator created beauty product.",
-				"This is the perfect place to bring that special someone",
-				"Get lost in this mirror perfect reflection"
+				"This was a landing page created for a final project for the Lord of the Rings movies."
 			],
 
 			imageTitle : [
@@ -64,7 +61,6 @@ var dynamicContent = {
 		},
 
 		lightboxThree: {
-			text : "Spring is one of the four conventional temperate seasons, following winter and preceding summer. There are various technical definitions of spring, but local usage of the term varies according to local climate, cultures and customs. When it is spring in the northern hemisphere, it will be autumn in the southern hemisphere. At the spring equinox, days are close to 12 hours long with day length increasing as the season progresses. Spring and 'springtime' refer to the season, and also to ideas of rebirth, rejuvenation, renewal, resurrection, and regrowth.",
 
 			headline : "Spring",
 
@@ -77,7 +73,7 @@ var dynamicContent = {
 
 			imageDescription : [
 				"This was completed for a young boy, who requested Harley",
-				"Have you ever seen a baby duck more adorable than this little fella? I didn't think so.",
+				"This is the first low-poly portrait I've done, I experimented with different techniques. Overall, I am happy with the end result.",
 				"these are amoungst the most beautiful flowers during the spring time",
 				"Gaze at the misty beauty that is the forest",
 				"This is the perfect place to bring that special someone",
@@ -87,15 +83,12 @@ var dynamicContent = {
 			imageTitle : [
 				"Low Poly Harley Quinn",
 				"Low Poly Spider-Man",
-				"Flowers",
-				"Sunset Forest",
-				"Wedding Destination",
-				"Reflective Lake"
+				"Girl with the Dragon Tattoo",
+				"Sunset Forest"
 			]
 		},
 
 		lightboxFour: {
-			text : "Spring is one of the four conventional temperate seasons, following winter and preceding summer. There are various technical definitions of spring, but local usage of the term varies according to local climate, cultures and customs. When it is spring in the northern hemisphere, it will be autumn in the southern hemisphere. At the spring equinox, days are close to 12 hours long with day length increasing as the season progresses. Spring and 'springtime' refer to the season, and also to ideas of rebirth, rejuvenation, renewal, resurrection, and regrowth.",
 
 			headline : "Spring",
 
@@ -107,21 +100,17 @@ var dynamicContent = {
 			],
 
 			imageDescription : [
-				"As you see, there are many beautiful butterflies flying around.",
-				"Have you ever seen a baby duck more adorable than this little fella? I didn't think so.",
-				"these are amoungst the most beautiful flowers during the spring time",
-				"Gaze at the misty beauty that is the forest",
-				"This is the perfect place to bring that special someone",
-				"Get lost in this mirror perfect reflection"
+				"For this final exam, we had to recreate a tunel with the ESPN logo at the end. Along with the camera movemnt.",
+				"For this project, we were required to design and create a modern hotel room.",
+				"This wine bottle was created to advertise the company that we had to create.",
+				"These beauty products are model after an inclass example."
 			],
 
 			imageTitle : [
-				"Butterflies",
-				"Baby Duck",
-				"Flowers",
-				"Sunset Forest",
-				"Wedding Destination",
-				"Reflective Lake"
+				"ESPN",
+				"Hotel Room",
+				"Wine",
+				"Beauty Products"
 			]
 		},
 
--- js/greensock.js
+++ js/greensock.js
@@ -4,7 +4,7 @@ var logo = document.querySelector(".homeLogo");
 var port = document.querySelector("#port");
 
 function moveLogo(){
-TweenMax.from(logo, 1.5, {opacity:0, y: -100, delay:5});
+TweenMax.from(logo, 1.5, {opacity:0, y: -100, delay:1});
 // TweenMax.to (logo,5,{opacity:1,y:100, ease: SlowMo.easeOut, delay: 3});
 }
 
